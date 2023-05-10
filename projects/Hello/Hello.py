d7b52398ab8d0af9769f4a1f2fadc55cdc97969b
kelechi <kelechi.agomuo@gmail.com>
2023-05-10 00:27:59 -0400

    just added some new print text.

--- projects/Hello/Hello.py
+++ projects/Hello/Hello.py
@@ -1,3 +1,6 @@
 # This is simple program to show how print statement works
 
 print('Hello Python World')
+
+print("hello, im so glad im using PyCharm")
+print("now im just printing random bullshit to the console")
